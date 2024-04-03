a42120ad08dd55473b2d60af33df8498c1353753
Hugo Fourmy <>
2024-04-03 17:09:02 +0200

    Update increase HOPPIP rate ROUTE_29

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1240,17 +1240,17 @@ JohtoGrassWildMons:
 	db 2, PIDGEY
 	db 2, SENTRET
 	db 3, PIDGEY
+	db 3, HOPPIP
 	db 3, SENTRET
 	db 2, RATTATA
-	db 3, HOPPIP
 	db 3, HOPPIP
 	; day
 	db 2, PIDGEY
 	db 2, SENTRET
 	db 3, PIDGEY
+	db 3, HOPPIP
 	db 3, SENTRET
 	db 2, RATTATA
-	db 3, HOPPIP
 	db 3, HOPPIP
 	; nite
 	db 2, HOOTHOOT
@@ -1259,7 +1259,7 @@ JohtoGrassWildMons:
 	db 3, RATTATA
 	db 2, RATTATA
 	db 3, HOOTHOOT
-	db 3, HOOTHOOT
+	db 3, HOPPIP
 	end_grass_wildmons
 
 	def_grass_wildmons ROUTE_30
